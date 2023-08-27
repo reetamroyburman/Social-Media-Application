29a5091443589a612b62f8814d727aa5ab6ad5c8
Reetam Roy Burman <contact.iamreetam@gmail.com>
2023-08-27 17:59:22 +0530

    update index
    update index

--- Server/index.js
+++ Server/index.js
@@ -19,8 +19,16 @@ const app = express();
 app.use(express.json({ limit: "10mb" }));
 app.use(morgan("common"));
 app.use(cookieParser());
-// let origin = 'http://localhost:3000';
+let origin = process.env.CLIENT_URL;
 console.log('here env', process.env.NODE_ENV);
+
+
+app.use(
+    cors({
+        credentials: true,
+        origin
+    })
+);
 
 app.use("/auth", authRouter);
 app.use("/posts", postsRouter);
